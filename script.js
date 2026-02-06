e0d3b6997e9d55e28a47f39a5ec45080a4a567ef
James27006 <pintojames2711@gmail.com>
2026-02-06 19:19:20 +0530

    First commit -To Do List project

---
+++ script.js
@@ -0,0 +1,25 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value.trim();
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    li.addEventListener("click", function () {
+        li.classList.toggle("completed");
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.style.marginLeft = "10px";
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
